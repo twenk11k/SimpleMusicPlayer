aae501d1392149dd95ea1c73f29adea8f8f4ab0a
Selim Demiral <selim_demiral@outlook.com>
2022-07-25 00:01:33 +0300

    Add txt_title, img_cover, img_favorite, seekBar, txt_duration_current, txt_duration_divide, txt_duration_total, and rating_bar views to fragment_music_play.xml and set up title TextView, cover ImageView, max of SeekBar, total duration TextView, and favorite ImageView in setUpUI method of MusicPlayFragment

--- app/src/main/java/com/twenk11k/simplemusicplayer/view/music_play/MusicPlayFragment.kt
+++ app/src/main/java/com/twenk11k/simplemusicplayer/view/music_play/MusicPlayFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import coil.load
 import com.twenk11k.simplemusicplayer.databinding.FragmentMusicPlayBinding
+import org.apache.commons.lang3.time.DurationFormatUtils
 
 class MusicPlayFragment : Fragment() {
 
@@ -21,5 +23,32 @@ class MusicPlayFragment : Fragment() {
     ): View {
         binding = FragmentMusicPlayBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpUI()
+    }
+
+    private fun setUpUI() {
+        binding.run {
+            args.let {
+                txtTitle.text = it.title
+                with(imgCover) {
+                    load(it.cover)
+                }
+                seekBar.max = it.totalDurationMs
+                txtDurationTotal.text =
+                    DurationFormatUtils.formatDuration(it.totalDurationMs.toLong(), "m:ss")
+                handleFavorite(it.isFavorite, it.title)
+            }
+        }
+    }
+
+    private fun handleFavorite(isFavorite: Boolean, title: String) {
+        binding.imgFavorite.run {
+            this.isSelected = isFavorite
+            // TODO handle click
+        }
     }
 }
