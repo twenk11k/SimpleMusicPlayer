f2c09299d45ef45a232afa34955978d4de8b3241
Selim Demiral <selim_demiral@outlook.com>
2022-07-25 00:15:25 +0300

    Apply AndroidEntryPoint and add MusicViewModel to MusicPlayFragment

--- app/src/main/java/com/twenk11k/simplemusicplayer/view/music_play/MusicPlayFragment.kt
+++ app/src/main/java/com/twenk11k/simplemusicplayer/view/music_play/MusicPlayFragment.kt
@@ -5,14 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import coil.load
 import com.twenk11k.simplemusicplayer.databinding.FragmentMusicPlayBinding
+import com.twenk11k.simplemusicplayer.presentation.viewmodel.music.MusicViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import org.apache.commons.lang3.time.DurationFormatUtils
 
+@AndroidEntryPoint
 class MusicPlayFragment : Fragment() {
 
     private lateinit var binding: FragmentMusicPlayBinding
+
+    private val viewModel by viewModels<MusicViewModel>()
 
     private val args: MusicPlayFragmentArgs by navArgs()
 
